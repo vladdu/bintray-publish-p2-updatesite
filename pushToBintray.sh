66ada7c7e213d1b801490428093bfd96760f80f9
Vlad Dumitrescu <vladdu55@gmail.com>
2014-12-03 11:42:13 +0100

    Allow uploading to different layouts
    - simple: a folder is created for each version
    - latest: upload to root, only latest version is visible from
    dl.bintray.com/user/repo

--- pushToBintray.sh
+++ pushToBintray.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-#Sample Usage: pushToBintray.sh username apikey owner repo package version
+#Sample Usage: pushToBintray.sh username apikey owner repo package version simple|latest
 API=https://api.bintray.com
 BINTRAY_CREDENTIAL=$1
 BINTRAY_OWNER=$2
 BINTRAY_REPO=$3
 PCK_NAME=$4
 PCK_VERSION=$5
+SITE_FORMAT=${6:-simple}
 
 function main() {
 deploy_updatesite
@@ -22,12 +23,26 @@ FILES=./*
 PLUGINDIR=./plugins/*
 FEATUREDIR=./features/*
 
+if [ $SITE_FORMAT -eq "simple" ]; then
+  # create a separate directory for each version
+  OUTPATH="$PCK_VERSION"
+else 
+  if [ $SITE_FORMAT -eq "latest" ]; then
+    # store all files at the root of the repository
+    OUTPATH=""
+  else
+    echo "Site format must be 'simple' or 'latest'"
+    exit 1
+  fi
+fi
+
+
 for f in $FILES;
 do
 if [ ! -d $f ]; then
   echo "Processing $f file..."
   curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
-    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$OUTPATH/$f;publish=1
   echo ""
 fi
 done
@@ -37,7 +52,7 @@ for f in $FEATUREDIR;
 do
   echo "Processing feature: $f file..."
   curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
-    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$OUTPATH/$f;publish=1
   echo ""
 done
 
@@ -45,9 +60,9 @@ echo "Processing plugin dir $PLUGINDIR file..."
 
 for f in $PLUGINDIR;
 do
-  echo "Processing plugin: $f file..."
+  echo "Processing plugin: $OUTPATH/$f file..."
   curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
-    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$OUTPATH/$f;publish=1
   echo ""
 done
 
