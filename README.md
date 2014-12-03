3e260cf0572780d1f8a1a76ac2e0186c3e2ee105
Vlad Dumitrescu <vladdu55@gmail.com>
2014-12-03 11:08:22 +0100

    One single argument for credentials and some cleanup

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 bintray-publish-p2-updatesite
 =============================
 
-Shell script which allows to upload a p2 update-site to bintray. To run, close this repository and make the pushToBintray.sh file executable.
+Shell script which allows to upload a p2 update-site to bintray. To run, clone this repository and make the pushToBintray.sh file executable.
 ```
    chmod +x pushToBintray.sh
 ```  
 Afterwards switch to your p2 repository you would like to upload and run the script:
 ```
-  pushToBintray.sh username apikey owner repo package version
+  pushToBintray.sh username:apikey owner repo package version
 ```
 
-The repository owner may be an organization, so different then the user that uploads the files.
+The repository owner may be an organization, other than the user that uploads the files.
 
--- pushToBintray.sh
+++ pushToBintray.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #Sample Usage: pushToBintray.sh username apikey owner repo package version
 API=https://api.bintray.com
-BINTRAY_USER=$1
-BINTRAY_API_KEY=$2
-BINTRAY_OWNER=$3
-BINTRAY_REPO=$4
-PCK_NAME=$5
-PCK_VERSION=$6
+BINTRAY_CREDENTIAL=$1
+BINTRAY_OWNER=$2
+BINTRAY_REPO=$3
+PCK_NAME=$4
+PCK_VERSION=$5
 
 function main() {
 deploy_updatesite
 }
 
 function deploy_updatesite() {
-echo "${BINTRAY_USER}"
-echo "${BINTRAY_API_KEY}"
+echo "${BINTRAY_CREDENTIAL}"
 echo "${BINTRAY_OWNER}"
 echo "${BINTRAY_REPO}"
 echo "${PCK_NAME}"
@@ -28,8 +26,8 @@ for f in $FILES;
 do
 if [ ! -d $f ]; then
   echo "Processing $f file..."
-  # take action on each file. $f store current file name
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+  curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
   echo ""
 fi
 done
@@ -38,7 +36,8 @@ echo "Processing features dir $FEATUREDIR file..."
 for f in $FEATUREDIR;
 do
   echo "Processing feature: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+  curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
   echo ""
 done
 
@@ -46,14 +45,16 @@ echo "Processing plugin dir $PLUGINDIR file..."
 
 for f in $PLUGINDIR;
 do
-   # take action on each file. $f store current file name
   echo "Processing plugin: $f file..."
-  curl -X PUT -T $f -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
+  curl -X PUT -T $f -u ${BINTRAY_CREDENTIAL} \
+    ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/$f;publish=1
   echo ""
 done
 
 echo "Publishing the new version"
-curl -X POST -u ${BINTRAY_USER}:${BINTRAY_API_KEY} https://api.bintray.com/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish -d "{ \"discard\": \"false\" }"
+curl -X POST -u ${BINTRAY_CREDENTIAL} \
+  ${API}/content/${BINTRAY_OWNER}/${BINTRAY_REPO}/${PCK_NAME}/${PCK_VERSION}/publish \
+  -d "{ \"discard\": \"false\" }"
 
 }
 
